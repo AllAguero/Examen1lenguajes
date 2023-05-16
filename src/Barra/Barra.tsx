dc6384ab4242882a7c7ea5b63b57a48e25bdb686
Allison <alliaguero300419@gamil.com>
2023-05-16 15:16:51 -0600

    ejemplo

---
+++ src/Barra/Barra.tsx
@@ -0,0 +1,17 @@
+import './Barra.css'
+function Barra() {
+    return (
+        <header id='navbar'> 
+         <div className='Classnavbar' id='Classnavbar'>
+             <div className='logo'><img src='' alt="" id='Logo' /></div>
+             <ul className='links'>
+                 <li><a href="#"></a>Inicio</li>
+                 <li><a href="#"></a>Sobre mi</li>
+                 <li><a href="#"></a>Contacto</li>
+             </ul>
+         </div>
+        </header>
+       );
+}
+
+export default Barra;
---
+++ src/Titulo/titulo.tsx
@@ -0,0 +1,15 @@
+import './titulo.css'
+function Titulo() {
+    return (
+        <div className="contenedor">
+            <div className="titulos">
+                <h1 id="Titulo">Allison Aguero</h1>
+                <h2 id="Titulo2">Front-end developer</h2>
+            </div>
+            <img src="./public/imagenes/ilustracionIA.png" alt="" id="ilustracion" />
+        </div>
+
+       );
+}
+
+export default Titulo;
